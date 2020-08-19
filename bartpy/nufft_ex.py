8406b49f3bfda6e7beb3cdedf073e81f365b1006
malits <maxlitster@gmail.com>
2020-08-19 15:53:24 -0700

    working on nufft

---
+++ bartpy/nufft_ex.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from ctypes import *
+import os
+import sys
+
+import numpy as np
+
+BART_PATH = os.environ['TOOLBOX_PATH']
+sys.path.insert(0, os.path.join(BART_PATH, 'python'))
+import cfl
+
+bartso = CDLL('./bart.so')
+
+dims = (c_long * 16)(1)
+dims[0] = 128
+dims[1] = 128
+dst = np.asfortranarray(np.empty((128, 128), dtype=np.complex64))
+d3 = c_bool(False)
+tstrs = (c_long * 16)(0)
+samples = None
+
+out = dst.ctypes.data_as(ctypes.POINTER(2 * ctypes.c_float))
